e498d7ec09daf5f7fc79ef3d742d1c7b91d3af9f
VAC2UM <nikita.golowanev@gmail.com>
2024-06-01 17:33:02 +0300

    Added some info

--- app/src/main/java/com/itproger/vacuummovies/Fragments/InfoFragment.java
+++ app/src/main/java/com/itproger/vacuummovies/Fragments/InfoFragment.java
@@ -1,20 +1,56 @@
 package com.itproger.vacuummovies.Fragments;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.itproger.vacuummovies.R;
 
 public class InfoFragment extends Fragment {
+    TextView creatorText;
+    TextView usindDesc;
+    TextView personalDesc;
+    ImageView githubBtn;
+    SharedPreferences sharedPref;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+
+        creatorText = rootView.findViewById(R.id.creator);
+        githubBtn = rootView.findViewById(R.id.github_btn);
+        usindDesc = rootView.findViewById(R.id.usingDesc);
+        personalDesc = rootView.findViewById(R.id.personalDesc);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        boolean isSuperUser = sharedPref.getBoolean(getString(R.string.saved_super_user_key), false);
+
+        githubBtn.setOnClickListener(view -> {
+            String url = "https://github.com/VAC2UM/VacuumMovies";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        creatorText.setText("Создатель приложения:\nстудент РТУ МИРЭА Голованев Никита");
+        usindDesc.setText("На главной странице вы увидите список фильмов, которые обозначены соответствующим постером. Сверху есть строка поиска фильма по его названию. Нажав на постер, вы перейдете на страницу с информацией о данном фильме.\n" +
+                "В левом нижнем углу вы увидите значок YouTube, нажав на который вы сможете посмотреть трейлер к данному фильму.");
+
+        if(isSuperUser){
+            personalDesc.setText("Вы являетесь админом.\nВам доступен новый функционал:\n");
+        }else{
+            personalDesc.setText("Вы обычный пользователь.\nВам доступен только вышеуказанный функционал.");
+        }
+        return rootView;
     }
 }
